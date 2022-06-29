c7847efd47626d57e2df8bea1f65eb0dfbd38267
Tahsinur Rahman <tahsinur72@gmail.com>
2022-06-30 01:47:10 +0600

    test changes

--- server/index.js
+++ server/index.js
@@ -8,13 +8,14 @@ const meds = require('./models/medicine')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 app.use('/medicine', require('./routes/api/insertMedicine'))
 
 mongoose.connect(
     process.env.DB_CONNECT_STRING, 
     { useNewUrlParser: true, useUnifiedTopology: true},
     (req,res) =>{
-        // console.log('Connected to database')
+        console.log('Connected to database')
     })
     var db = mongoose.connection;
 app.listen(process.env.PORT, ()=>{
